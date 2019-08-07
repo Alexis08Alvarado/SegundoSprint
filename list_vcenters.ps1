0721bed9818a807161db5323aae96fad51c9361d
Alexis Alvarado <aalvarado@canvia.com>
2019-08-07 11:53:36 -0500

    Segundo Sprint 100%

--- list_vcenters.ps1
+++ list_vcenters.ps1
@@ -6,5 +6,5 @@ param(
 )
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false -Scope User -ParticipateInCEIP:$false | Out-Null 
 Connect-VIServer -Server $vcenter -Protocol https -User $username -Password $password | Out-Null
-Get-VM | Where-Object {$_.Notes -like "*103107*"} | Select-Object Name, PowerState, @{N="DnsName"; E={$_.ExtensionData.Guest.Hostname}}, NumCpu, MemoryGB | ConvertTo-Csv  
+Get-VM | Where-Object {$_.Notes -like "*103107*"} | Select-Object  @{ expression={$_.Name}; label='VM' }, PowerState, @{N="DnsName"; E={$_.ExtensionData.Guest.Hostname}}, @{ expression={$_.NumCpu}; label='CPUs' }, MemoryGB | ConvertTo-Csv 
 
